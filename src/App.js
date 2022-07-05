fb143c323e7a040dd986af6a9772f6ded6eccf48
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-05 13:12:08 +0100

    Set up components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I Edited
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import Calculator from './components/Calculator';
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// eslint-disable-next-line react/prefer-stateless-function
+class Calculator extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="main">
+          <h1>Hello world</h1>
+        </div>
+      </>
+    );
+  }
+}
+export default Calculator;
