a0b206208d4726b02652e1cb3b41240e5e754f26
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-05 15:44:39 +0100

    Created UI calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,8 +4,29 @@ class Calculator extends React.Component {
   render() {
     return (
       <>
-        <div className="main">
-          <h1>Hello world</h1>
+        <div className="container">
+          <div className="wrapper">
+            <div className="screen"> 0</div>
+            <div className="btn light-gray">AC</div>
+            <div className="btn light-gray">%</div>
+            <div className="btn light-gray">+/-</div>
+            <div className="btn orange">/</div>
+            <div className="btn">7</div>
+            <div className="btn">8</div>
+            <div className="btn">9</div>
+            <div className="btn orange">x</div>
+            <div className="btn">4</div>
+            <div className="btn">5</div>
+            <div className="btn">6</div>
+            <div className="btn orange">-</div>
+            <div className="btn">1</div>
+            <div className="btn">2</div>
+            <div className="btn">3</div>
+            <div className="btn orange">+</div>
+            <div className="btn zero">0</div>
+            <div className="btn">.</div>
+            <div className="btn orange">=</div>
+          </div>
         </div>
       </>
     );
