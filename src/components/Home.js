df1ab62a8483320ce1d1ba801b50f411387ac04c
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-12 15:09:07 +0100

    Add Home file

---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome to our page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Earum ipsum tempore, ea soluta
+        tenetur expedita dignissimos similique deleniti doloribus fugiat!
+        Necessitatibus, sint
+        doloribus deserunt voluptates impedit cumque alias,
+        aperiam illo dolore explicabo iure
+        culpa distinctio inventore perspiciatis sunt ipsam
+        atque.Lorem ipsum dolor sit amet consectetur, adipi
+        sicing elit. Earum ipsum tempore, ea soluta
+        tenetur expedita dignissimos similique deleniti
+        doloribus fugiat! Necessitatibus, sint
+        doloribus deserunt voluptates impedit cumque alias,
+        aperiam illo dolore explicabo iure
+        culpa distinctio inventore perspiciatis sunt ipsam a
+        tque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Earum ipsum tempore, ea soluta
+        tenetur expedita dignissimos similique deleniti doloribus fugiat!
+        Necessitatibus, sint
+        doloribus deserunt voluptates impedit cumque alias,
+        aperiam illo dolore explicabo iure
+        culpa distinctio inventore perspiciatis sunt ipsam
+        atque.Lorem ipsum dolor sit amet consectetur, adipi
+        sicing elit. Earum ipsum tempore, ea soluta
+        tenetur expedita dignissimos similique deleniti
+        doloribus fugiat! Necessitatibus, sint
+        doloribus deserunt voluptates impedit cumque alias,
+        aperiam illo dolore explicabo iure
+        culpa distinctio inventore perspiciatis sunt ipsam a
+        tque.
+      </p>
+    </div>
+  );
+}
+export default Home;
